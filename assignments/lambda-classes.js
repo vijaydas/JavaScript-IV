03b1e04e94d15a56d719d0b1ec9e995d32718c79
Vijay Das <vijaytdas@gmail.com>
2019-04-04 17:22:01 -0500

    add Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,15 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(PersonAttributes) {
+        this.name = PersonAttributes.name;
+        this.age = PersonAttributes.age;
+        this.location = PersonAttributes.location;
+        this.gender = PersonAttributes.gender;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+
